727aa25b7d4f9e70c9203fb569b0086155e2789f
PlayTube <prankking592@gmail.com>
2026-08-08 09:56:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ except Exception as e:
 # ==========================================
 # Constants
 # ==========================================
-MAIN_CHANNEL = "@income_box1"
+MAIN_CHANNEL = "@income_box_x"
 SUPPORT_CHANNEL = os.getenv('SUPPORT_CHANNEL_ID', '-1003951413076')
 BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
